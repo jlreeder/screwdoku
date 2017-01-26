6868407ea5e2e6f6968346f90afaa3f37a25b09b
Jason Reeder <info@jasonreeder.com>
2017-01-26 15:28:00 -0800

    Fix validation logic

--- sudoku.rb
+++ sudoku.rb
@@ -75,8 +75,8 @@ class SudokuGame
 
   def valid_pos?(pos)
     if pos.is_a?(Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+      pos.length == 2 &&
+      pos.all? { |x| x.between?(0, board.size - 1) }
       return true
     else
       get_pos
